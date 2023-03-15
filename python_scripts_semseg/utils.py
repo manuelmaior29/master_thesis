59e67c4e6ef2c4fa773d60f9412c055be70187aa
Manu <manuelmaior.29@gmail.com>
2023-03-15 21:49:48 +0200

    Implemented iou utility function for sem. seg.

--- python_scripts_semseg/utils.py
+++ python_scripts_semseg/utils.py
@@ -1,5 +1,16 @@
 import torch
-import torch.nn as nn
+import sys
+from sklearn.metrics import confusion_matrix
+
+def iou(predictions, targets, num_classes, smooth=sys.float_info.epsilon):
+    confusion_mat = confusion_matrix(targets.flatten(), predictions.flatten(), labels=range(num_classes))
+    ious = []
+    for c in range(num_classes):
+        intersection = confusion_mat[c, c]
+        union = confusion_mat[c, :].sum() + confusion_mat[:, c].sum() - intersection
+        ious.append((intersection + smooth) / (union + smooth))
+    return ious
+
 def add_weight_decay(net, l2_value, skip_list=()):
     decay, no_decay = [], []
     for name, param in net.named_parameters():
