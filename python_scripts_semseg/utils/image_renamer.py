fbd3c736b7efe9b1fd4eabffa92e2b0581d69bcf
unknown <manuelmaior.29@gmail.com>
2023-04-27 21:18:04 +0300

    Modified image renaming utility script

--- python_scripts_semseg/utils/image_renamer.py
+++ python_scripts_semseg/utils/image_renamer.py
@@ -1,9 +1,19 @@
 import shutil
 import os
+from PIL import Image
 
 index = 0
-path = r'C:\Users\Manuel\Projects\GitHub_Repositories\master_thesis\datasets\real\val\rgb'
+path = r'E:\Datasets\synthetic\train\rgb'
+print(len(os.listdir(path)))
 for file in os.listdir(path):
     if file.endswith('.png'):
-        os.rename(f'{path}/{file}', f'{path}/real_rgb_{index}.png')
-        index += 1
+        # index = int(file.split('.')[0].split('_')[-1])
+        index = int(file.split('_')[0])
+        print(index)
+
+        new_file = f'synthetic_rgb_{index}.png'
+        os.rename(f'{path}/{file}', f'{path}/{new_file}')
+        
+        # image = Image.open(path + file)
+        # image_resized = image.resize((1434, 717))
+        # image_resized.save(path + file)
